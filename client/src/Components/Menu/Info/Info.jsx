d17f96553c70305d11b68b7af1da903f8f1b23d2
Sebastian Ahlman <hawaiilive@outlook.com>
2021-05-27 15:45:10 +0200

    now updates new pixels

--- client/src/Components/Menu/Info/Info.jsx
+++ client/src/Components/Menu/Info/Info.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 import "./Info.css";
 
 function Info({viewing, setViewing, info}) {
+    console.log(info);
+    console.log("--Info--");
     return (
         <div className="Info">
             <h2>Pixels:</h2>
             <h3>{"Total Pixels: " + info.pixels}</h3>
-            <h3>New Pixels: 20 / h</h3>
+            <h3>{"New Pixels: " + Math.round(parseFloat(info.standardPixels) + parseFloat(info.factories) * parseFloat(info.factoryMult) * 60) +" / h"}</h3>
             <button onClick={()=>setViewing(prev=>!prev)}>{viewing ? "Place Pixel" : "View Canvas"}</button>
         </div>
     )
