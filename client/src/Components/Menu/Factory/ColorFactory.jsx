ed38e513615580d432f439b403161bfca2baa115
Sebastian Ahlman <hawaiilive@outlook.com>
2021-05-27 15:45:40 +0200

    now updates factories  and can purchase

--- client/src/Components/Menu/Factory/ColorFactory.jsx
+++ client/src/Components/Menu/Factory/ColorFactory.jsx
@@ -1,21 +1,34 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 
 import "./Factory.css";
 
-function ColorFactory() {
+function ColorFactory({info, socket}) {
     const colors = ["White","Black","Red","Blue","Yellow","Green","Purple","Gold"]
     const [unlockedColors,setUnlocedColors] = useState(2);
     const [colorPrice, setColorPrice] = useState(100);
     const increaeCost = 1.8;
 
     const unlockPixel = ()=>{
-        setColorPrice(prev=>parseInt(prev*increaeCost));
-        setUnlocedColors(prev=>prev+1);
+        if(info.pixels >= colorPrice){
+            socket.emit("PurchaseColor",{price:colorPrice});
+            setColorPrice(prev=>parseInt(prev*increaeCost));
+            setUnlocedColors(prev=>prev+1);
+        }
     }
+
+    useEffect(()=>{
+        setUnlocedColors(info.colors);
+
+        let currentPrice = 100;
+        for(let i = 0; i < info.colors; i++){
+            currentPrice = currentPrice * increaeCost;
+        }
+        setColorPrice(Math.round(currentPrice));
+    },[info])
     
     return (
         <div className="ColorFactory">
-            <h3>{"Unlocked Colors: " + "3" + " / 15"}</h3>
+            <h3>{`Unlocked Colors: ${info.colors} / 15`}</h3>
             <div className="UnlockedColors">
                 {[...Array(unlockedColors)].map((item, index)=>{
                     return(<div className={"Color " + colors[index]} key={index}></div>)
